591f3dff2154e641bc2d9d8a0cf8e7cebf5013fa
llmel <contatoleonardomelo@hotmail.com>
2019-04-25 22:06:06 -0300

    Validação completa dos dados de cadastro

--- site/Boots/validation.cadastro.js
+++ site/Boots/validation.cadastro.js
@@ -1,4 +1,4 @@
-function validacao_cadastro ( cp1 , cp2 , cp3 , cp4 , cp5 , cp6 , cp7 , cp8 , cp9 , cp10 ) {
+function validacao_cadastro ( cp1 , cp2 , cp3 , cp4 , cp5 , cp6 , cp7 , cp8 , cp9 , cp10 , cp11 , cp12 ) {
     var c1 = cp1.value ;
     var c2 = cp2.value ;
     var c3 = cp3.value ;
@@ -9,7 +9,9 @@ function validacao_cadastro ( cp1 , cp2 , cp3 , cp4 , cp5 , cp6 , cp7 , cp8 , cp
     var c8 = cp8.value ;
     var c9 = cp9.value ;
     var c10 = cp10.value ;
-    if ( c1 == "" || c2 == "" || c3 == "" || c4 == "" || c5 == "" || c6 == "" || c7 == "" || c8 == "" || c9 == "" || c10 == "" || p_fisica.checked == true ) {
+    var c11 = cp11.checked ;
+    var c12 = cp12.checked ;
+    if ( c1 == "" || c2 == "" || c3 == "" || c4 == "" || c5 == "" || c6 == "" || c7 == "" || c8 == "" || c9 == "" || c10 == "" ) {
         alert ( "Preencha todos os campos" ) ;
         c1 == "" ? cp1.className = " new-style form-control-user " : cp1.className = " form-control form-control-user " ;
         c2 == "" ? cp2.className = " new-style form-control-user " : cp2.className = " form-control form-control-user " ;
@@ -22,8 +24,24 @@ function validacao_cadastro ( cp1 , cp2 , cp3 , cp4 , cp5 , cp6 , cp7 , cp8 , cp
         c9 == "" ? cp9.className = " new-style form-control-user " : cp9.className = " form-control form-control-user " ;
         c10 == "" ? cp10.className = " new-style form-control-user " : cp10.className = " form-control form-control-user " ;
         
+    } else if ( c11 || c12 ) {
+        if ( c11 && c12 ) {
+            alert ( "Preencha apenas uma das opções de tipo de pessoa" ) ;
+        } else {
+            location.href = "login.html" ;
+        }
     } else {
-        location.href = "login.html" ;
+        alert ( "Escolha o tipo de pessoa" ) ;
+        cp1.className = " form-control form-control-user " ;
+        cp2.className = " form-control form-control-user " ;
+        cp3.className = " form-control form-control-user " ;
+        cp4.className = " form-control form-control-user " ;
+        cp5.className = " form-control form-control-user " ;
+        cp6.className = " form-control form-control-user " ;
+        cp7.className = " form-control form-control-user " ;
+        cp8.className = " form-control form-control-user " ;
+        cp9.className = " form-control form-control-user " ;
+        cp10.className = " form-control form-control-user " ;
     }
 
 }
